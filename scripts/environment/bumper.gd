e019311f4fb0c3043391f253c8413fed27746bd6
cxhuy <alvinkang1@gmail.com>
2023-10-21 01:10:59 -0500

    feat: add Balls group

--- scripts/environment/bumper.gd
+++ scripts/environment/bumper.gd
@@ -2,6 +2,6 @@ extends RigidBody2D
 
 
 func _on_body_entered(body):
-	if body.name == "Ball":
+	if body.is_in_group("Balls"):
 		var direction: Vector2 = (position - body.position).normalized()
 		body.apply_impulse(direction * -1500)
