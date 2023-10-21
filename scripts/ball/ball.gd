004c6770d2a15217ae4f8dfb4b1a08287fc4bd95
cxhuy <alvinkang1@gmail.com>
2023-10-21 12:05:59 -0500

    refactor: create function shootBallSmall

--- scripts/ball/ball.gd
+++ scripts/ball/ball.gd
@@ -4,6 +4,10 @@ var ballSmall := preload("res://scenes/ballSmall.tscn")
 
 
 func _on_body_entered(body):
+	shootBallSmall()
+
+
+func shootBallSmall():
 	var ballSmallInstance := ballSmall.instantiate()
 	add_child(ballSmallInstance)
 	ballSmallInstance.set_global_position(self.global_position)	
