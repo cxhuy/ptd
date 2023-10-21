6e2b5f57cf148047b716760bea75abd2b47fc518
cxhuy <alvinkang1@gmail.com>
2023-10-21 16:09:48 -0500

    feat: add bullet damage

--- scripts/tower/bullet.gd
+++ scripts/tower/bullet.gd
@@ -2,8 +2,13 @@ extends Area2D
 
 
 var speed: int = 2000
+var damage: int = 10
 
 func _process(delta):
 	global_position += \
 	Vector2(cos(deg_to_rad(self.global_rotation_degrees)), \
 			sin(deg_to_rad(self.global_rotation_degrees))) * speed * delta
+			
+
+func _on_body_entered(body):
+	body.damage(damage)
