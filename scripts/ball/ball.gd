00992b8de5e36bcdcdba02c415e7ab4328430251
cxhuy <alvinkang1@gmail.com>
2023-10-21 12:05:09 -0500

    feat: add impulse to ball small

--- scripts/ball/ball.gd
+++ scripts/ball/ball.gd
@@ -7,4 +7,6 @@ func _on_body_entered(body):
 	var ballSmallInstance := ballSmall.instantiate()
 	add_child(ballSmallInstance)
 	ballSmallInstance.set_global_position(self.global_position)	
-	
+	var direction = Vector2(cos(deg_to_rad(self.global_rotation_degrees + randf_range(-5, 5))), \
+							sin(deg_to_rad(self.global_rotation_degrees + randf_range(-5, 5))))
+	ballSmallInstance.apply_impulse(direction * -1000)
