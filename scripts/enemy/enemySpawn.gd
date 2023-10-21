ebb7c63e13749bea492c9a88a27f6967631360bb
cxhuy <alvinkang1@gmail.com>
2023-10-20 23:46:49 -0500

    fix: set enemy path loop to false

--- scripts/enemy/enemySpawn.gd
+++ scripts/enemy/enemySpawn.gd
@@ -5,6 +5,7 @@ var enemy = preload("res://scenes/enemy/enemy.tscn")
 
 func _on_spawn_timer_timeout():
 	var enemyPath: PathFollow2D = PathFollow2D.new()
+	enemyPath.set_loop(false)
 	var enemyInstance := enemy.instantiate()
 	enemyPath.add_child(enemyInstance)
 	self.add_child(enemyPath)
