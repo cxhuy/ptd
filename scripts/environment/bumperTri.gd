1e706300c6c6470bee6e801eee5a05b309abe62f
cxhuy <alvinkang1@gmail.com>
2023-10-21 15:05:42 -0500

    feat: change wall and bumperTri bounce

--- scripts/environment/bumperTri.gd
+++ scripts/environment/bumperTri.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
 
-func _on_body_entered(body):
-	if body.is_in_group("Balls"):
-		var direction: Vector2 = (position - body.position).normalized()
-		body.apply_impulse(direction * 1500)
+#func _on_body_entered(body):
+#	if body.is_in_group("Balls"):
+#		var direction: Vector2 = (self.global_position - body.global_position).normalized()
+#		print(direction)
+#		body.apply_impulse(Vector2(direction.x * -500, direction.y * -1000))
