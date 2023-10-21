7fab019bb913640786c0509072cb200e26d7a16d
cxhuy <alvinkang1@gmail.com>
2023-10-20 21:58:06 -0500

    refactor: refactor bumper script

--- scripts/environment/bumper.gd
+++ scripts/environment/bumper.gd
@@ -3,7 +3,5 @@ extends RigidBody2D
 
 func _on_body_entered(body):
 	if body.name == "Ball":
-		var direction = position - body.position
-		direction = direction.normalized()
-		var impulse = direction * -1500
-		body.apply_impulse(impulse)
+		var direction: Vector2 = (position - body.position).normalized()
+		body.apply_impulse(direction * -1500)
