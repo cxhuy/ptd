128951e8830b2865919e6fbc110344a4cda8b7dd
cxhuy <alvinkang1@gmail.com>
2023-10-21 16:09:33 -0500

    feat: add enemy damage function

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -1,10 +1,26 @@
 extends CharacterBody2D
 
-var speed: int = 100
+var enemyId: int = 0
+var enemy: Dictionary = {
+	0: {"speed": 50, "health": 10},
+}
+var speed: int
+var health: int
 
 
-func _process(delta):
+func _ready():
+	self.speed = enemy[enemyId]["speed"]
+	self.health = enemy[enemyId]["health"]
+
+
+func _process(delta):	
 	get_parent().set_progress(get_parent().get_progress() + speed * delta)	
 	self.global_rotation_degrees = 0
 	if get_parent().get_progress_ratio() == 1:
+		self.queue_free()
+
+
+func damage(damageAmount: int):
+	self.health -= damageAmount
+	if self.health <= 0:
 		self.queue_free()
