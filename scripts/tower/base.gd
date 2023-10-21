73097cb7b9ab103024395d3b35b4c1df76457f10
cxhuy <alvinkang1@gmail.com>
2023-10-21 01:20:18 -0500

    feat: add tower bullet fire

--- scripts/tower/base.gd
+++ scripts/tower/base.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 @onready var anim = get_node("../SwitchSprite")
 
+var bullet := preload("res://scenes/tower/bullet.tscn")
 var switchDuration: int = 0
 
 
@@ -14,7 +15,11 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	if body.name == "Ball":
+	if body.is_in_group("Balls"):
 		var direction: Vector2 = (position - body.position).normalized()
 		body.apply_impulse(direction * 1500)
 		switchDuration = 5
+		var bulletInstance = bullet.instantiate()
+		bulletInstance.set_global_position(get_parent().global_position)
+		bulletInstance.set_global_rotation_degrees(get_parent().global_rotation_degrees)
+		get_tree().get_root().get_node("Game").get_node("Projectiles").add_child(bulletInstance)
