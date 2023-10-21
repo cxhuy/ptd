8c712d39b35de5b177bc0f21d316ef9ac2b4b5c8
cxhuy <alvinkang1@gmail.com>
2023-10-20 23:47:21 -0500

    feat: free enemy when reaching end of path

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -4,5 +4,7 @@ var speed: int = 100
 
 
 func _process(delta):
-	get_parent().set_progress(get_parent().progress + speed * delta)	
+	get_parent().set_progress(get_parent().get_progress() + speed * delta)	
 	self.global_rotation_degrees = 0
+	if get_parent().get_progress_ratio() == 1:
+		self.queue_free()
