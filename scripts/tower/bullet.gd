030ac2941abc8a5006ff8418a9658e05aff5426c
cxhuy <alvinkang1@gmail.com>
2023-10-21 01:19:41 -0500

    fix: fix bullet direction

--- scripts/tower/bullet.gd
+++ scripts/tower/bullet.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 
-var speed: int = 1000
+var speed: int = 2000
 
 func _process(delta):
-	global_position += Vector2(cos(self.rotation_degrees), sin(self.rotation_degrees)) * speed * delta
+	global_position += \
+	Vector2(cos(deg_to_rad(self.global_rotation_degrees)), \
+			sin(deg_to_rad(self.global_rotation_degrees))) * speed * delta
