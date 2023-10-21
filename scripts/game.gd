23a0ce07f41683e06c491834c1e7c99f861e0a34
cxhuy <alvinkang1@gmail.com>
2023-10-21 15:30:46 -0500

    feat: change ball spawn position

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,5 +6,5 @@ var ball = preload("res://scenes/ball.tscn")
 func _input(event):
 	if Input.is_action_pressed("addBall"):
 		var ballInstance = ball.instantiate()
-		ballInstance.set_global_position(Vector2(750, 100))
+		ballInstance.set_global_position(Vector2(616, 583))
 		add_child(ballInstance)
