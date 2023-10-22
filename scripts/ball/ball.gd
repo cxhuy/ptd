4773ebc75915606b95e24c117c02a5be74a1e7fa
cxhuy <alvinkang1@gmail.com>
2023-10-22 01:04:52 -0500

    feat: free ball when exit screen

--- scripts/ball/ball.gd
+++ scripts/ball/ball.gd
@@ -14,3 +14,7 @@ func shootBallSmall():
 	var direction = Vector2(cos(deg_to_rad(self.global_rotation_degrees + randf_range(-5, 5))), \
 							sin(deg_to_rad(self.global_rotation_degrees + randf_range(-5, 5))))
 	ballSmallInstance.apply_impulse(direction * -1000)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	self.queue_free()
