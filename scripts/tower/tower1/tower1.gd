6f223ffacee4cebf9d93cc4bdaf70aa20947f952
cxhuy <alvinkang1@gmail.com>
2023-10-22 05:05:00 -0500

    fix: fix errors

--- scripts/tower/tower1/tower1.gd
+++ scripts/tower/tower1/tower1.gd
@@ -20,8 +20,8 @@ func _on_base_body_entered(body):
 		body.apply_impulse(direction * -1500)
 		switchDuration = 20
 		var explosionInstance := explosion.instantiate()
-		add_child(explosionInstance)
-		explosionInstance.set_global_position(self.global_position)
+		call_deferred("add_child", explosionInstance)
+		explosionInstance.set_global_position(self.global_position)				
 		var tween = create_tween().set_trans(Tween.TRANS_CIRC)
 		tween.tween_property(anim, "scale", Vector2(0.3, 0.3), 0.07)	
 		await get_tree().create_timer(0.2).timeout
