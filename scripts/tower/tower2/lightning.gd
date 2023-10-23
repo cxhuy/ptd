aba5ae8e6649f154aff5950e4f8387dee5100e2f
cxhuy <alvinkang1@gmail.com>
2023-10-23 00:35:28 -0500

    feat: add lightning effect

--- scripts/tower/tower2/lightning.gd
+++ scripts/tower/tower2/lightning.gd
@@ -7,7 +7,19 @@ var enemiesSortedByProgress
 
 func _ready():	
 	self.global_position = get_parent().global_position
+	
 	lightningEffect.add_point(Vector2(0, 0))
 	
-	for enemy in enemiesSortedByProgress:
-		lightningEffect.add_point(enemy.global_position - self.global_position)
+	for i in range(enemiesSortedByProgress.size()):
+		var currPointPos: Vector2 = enemiesSortedByProgress[i].global_position - self.global_position
+		lightningEffect.add_point(currPointPos)	
+		if i != enemiesSortedByProgress.size() - 1:
+			var nextPointPos: Vector2 = enemiesSortedByProgress[i + 1].global_position - self.global_position
+			var midPointPos: Vector2 = (currPointPos + nextPointPos) / 2
+			lightningEffect.add_point(midPointPos + Vector2(randi_range(-50, 50), randi_range(-50, 50)))
+		
+	var tween = create_tween().set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "modulate:a", 0, 1)	
+	await get_tree().create_timer(1).timeout
+	self.queue_free()
+
