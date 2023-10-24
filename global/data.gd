f24b8ed4827dfd4d4c1f8dc349c5c1fd7fd72f77
cxhuy <alvinkang1@gmail.com>
2023-10-23 20:45:26 -0500

    feat: add tower upgrades and levels to global data

--- global/data.gd
+++ global/data.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+# Number of towers required for upgrading level
+var upgradeRequired: Array[int] = [0, 0, 2, 4, 10, 20, 50, 100, 200]
+
+# Number of towers in inventory
 var towerQuantity: Array[int] = [3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+# Level of each towers
+var towerLevels: Array[int] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+# Stats of towers
 var towerStats: Dictionary = {
 	1: {
 		"damage": 10
