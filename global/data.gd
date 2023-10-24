5e0fdda407e2a748996be5963ddf57fa92edcb4c
cxhuy <alvinkang1@gmail.com>
2023-10-24 15:35:51 -0500

    feat: add tower limit global data

--- global/data.gd
+++ global/data.gd
@@ -4,6 +4,8 @@ var currentStage: int = 1
 var currentWave: int = 1
 var currentHealth: int = 5
 
+var towerLimit: int = 3
+
 # Number of towers required for upgrading level
 const upgradeRequired: Array[int] = [0, 2, 4, 10, 20, 50, 100, 200, 400]
 
