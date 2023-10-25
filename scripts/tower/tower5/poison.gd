401f957bee4a083612fc4b14a7a205f54d111383
cxhuy <alvinkang1@gmail.com>
2023-10-25 15:35:25 -0500

    feat: add tower5 poison tween

---
+++ scripts/tower/tower5/poison.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+func _ready():
+	self.global_position = get_parent().global_position
+	self.scale = Vector2(0, 0)
+	var tween = create_tween().set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.4)	
+	await get_tree().create_timer(0.4).timeout
+	tween = create_tween().set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "modulate:a", 0, 0.4)	
+	await get_tree().create_timer(0.4).timeout
+	queue_free()
+
+
+func _on_body_entered(body):
+	pass
