9f2a179105d2ba0c3fd4880f6387202ae06c336b
cxhuy <alvinkang1@gmail.com>
2023-10-25 14:44:43 -0500

    feat: tween opacity and time of tower3 beam

--- scripts/tower/tower3/beam.gd
+++ scripts/tower/tower3/beam.gd
@@ -3,12 +3,14 @@ extends Area2D
 
 func _ready():
 	var tween = create_tween().set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "rotation_degrees", self.rotation_degrees + 60, 1)
+	tween.tween_property(self, "rotation_degrees", self.rotation_degrees + 60, 0.5)
 	var tween2 = create_tween().set_ease(Tween.EASE_OUT)
-	tween2.tween_property(self, "scale", Vector2(2, 2), 1)	
-	await get_tree().create_timer(1).timeout
+	tween2.tween_property(self, "scale", Vector2(2, 2), 0.5)
+	var tween3 = create_tween().set_ease(Tween.EASE_OUT)
+	tween2.tween_property(self, "modulate:a", 0, 0.5)
+	await get_tree().create_timer(0.5).timeout
 	self.queue_free()
 
 
 func _on_body_entered(body):
-	body.damage(Data.towerData[2]["stats"]["damage"][Data.towerData[3]["level"]])
+	body.damage(Data.towerData[3]["stats"]["damage"][Data.towerData[3]["level"]])
