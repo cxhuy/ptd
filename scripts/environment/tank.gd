b04de0efc2b0da8c10de4d6414464cc51e348552
cxhuy <alvinkang1@gmail.com>
2023-10-25 03:35:58 -0500

    feat: change tank ball send y pos to 0

--- scripts/environment/tank.gd
+++ scripts/environment/tank.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if local_shape_index == 1:
-		call_deferred("send", body, Vector2(960, 15) + \
+		call_deferred("send", body, Vector2(960, 0) + \
 									Vector2((body.global_position.x - 1630) * 750 / 350, 0))
 
 
