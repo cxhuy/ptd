5f23cc9a99afbebe39585412a99cde54aab61b71
cxhuy <alvinkang1@gmail.com>
2023-10-25 03:00:07 -0500

    feat: send tank ball to right position

--- scripts/environment/tank.gd
+++ scripts/environment/tank.gd
@@ -3,7 +3,9 @@ extends RigidBody2D
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if local_shape_index == 1:
-		call_deferred("send", body, Vector2(960, 500))
+		call_deferred("send", body, Vector2(960, 15) + \
+									Vector2((body.global_position.x - 1630) * 750 / 350, 0))
+
 
 func send(body, position):
 	body.global_position = position
