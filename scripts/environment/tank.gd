982088af030559f45b7fe467d9e4e019b7eab1e7
cxhuy <alvinkang1@gmail.com>
2023-10-25 03:48:53 -0500

    feat: add empty tank button bind

--- scripts/environment/tank.gd
+++ scripts/environment/tank.gd
@@ -1,8 +1,10 @@
 extends RigidBody2D
 
+var emptyTank: bool = false
+
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	if local_shape_index == 1:
+	if local_shape_index == 1 and emptyTank:
 		call_deferred("send", body, Vector2(960, 0) + \
 									Vector2((body.global_position.x - 1630) * 750 / 350, 0))
 
