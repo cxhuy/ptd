984cbe5ef673c6572844d90d92cd7fbe99429222
cxhuy <alvinkang1@gmail.com>
2023-10-25 15:15:53 -0500

    feat: tower4 bullet damage scales by distance

--- scripts/tower/tower4/bullet.gd
+++ scripts/tower/tower4/bullet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var speed: int = 2000
+var damageIncrease: int = 0
 
 
 func _ready():
@@ -9,13 +10,17 @@ func _ready():
 
 
 func _process(delta):
+	damageIncrease += 1
 	self.global_position += \
 		Vector2(cos(deg_to_rad(self.global_rotation_degrees)), \
 				sin(deg_to_rad(self.global_rotation_degrees))) * speed * delta
 
 
 func _on_body_entered(body):
-	body.damage(Data.towerData[4]["stats"]["damage"][Data.towerData[4]["level"]])
+	var damage: int = Data.towerData[4]["stats"]["damage"][Data.towerData[4]["level"]]
+	var maxDamage: int = Data.towerData[4]["stats"]["damage"][Data.towerData[4]["level"]] * 2
+	var increasedDamage: int = min(damage * (1 + 0.05 * damageIncrease), maxDamage)
+	body.damage(increasedDamage)
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
