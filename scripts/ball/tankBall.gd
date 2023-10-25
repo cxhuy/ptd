49fab52c154547e4bf9e0fc09f41c38bbf892431
cxhuy <alvinkang1@gmail.com>
2023-10-25 03:30:36 -0500

    feat: tank ball acts as normal ball when sent

--- scripts/ball/tankBall.gd
+++ scripts/ball/tankBall.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
 var velocity: Vector2 = Vector2(cos(randi_range(0, 360)), sin(randi_range(0, 360))) * 1000
+var sent: bool = false
 
 func _physics_process(delta):
-	var collision = move_and_collide(velocity * delta)
+	if !sent:
+		var collision = move_and_collide(velocity * delta)
 
-	if collision:
-		velocity = velocity.bounce(collision.get_normal()) 
+		if collision:
+			velocity = velocity.bounce(collision.get_normal()) 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	self.queue_free()
--- scripts/environment/tank.gd
+++ scripts/environment/tank.gd
@@ -7,5 +7,10 @@ func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index)
 									Vector2((body.global_position.x - 1630) * 750 / 350, 0))
 
 
-func send(body, position):
+func send(body: RigidBody2D, position):
+	var newPhysicsMaterial: PhysicsMaterial = PhysicsMaterial.new()
+	newPhysicsMaterial.friction = 0.3
+	newPhysicsMaterial.bounce = 0.1
+	body.physics_material_override = newPhysicsMaterial
+	body.sent = true
 	body.global_position = position
