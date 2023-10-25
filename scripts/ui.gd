a21871c31dcbc70505fb2483a3a9cd48e7c21cd3
cxhuy <alvinkang1@gmail.com>
2023-10-25 17:35:44 -0500

    rename: move fountain tower out of the list

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -7,7 +7,7 @@ var towerToDelete
 
 
 func _ready():
-	for i in range(11):
+	for i in range(10):
 		var towerAddInstance = towerAdd.instantiate()
 		var tower = load("res://scenes/tower/tower" + str(i + 1) + "/tower" + str(i + 1) + ".tscn")		
 		towerAddInstance.tower = tower		
