76a87ddc12540f639732325ead270aabfa11f135
cxhuy <alvinkang1@gmail.com>
2023-10-26 00:27:50 -0500

    feat: add help button styles

--- scripts/help.gd
+++ scripts/help.gd
@@ -6,6 +6,8 @@ var helpIndex: int = 1
 
 func _on_next_button_pressed():
 	helpIndex += 1
+	if helpIndex == 4:
+		$NextButton.text = "Go back"
 	if helpIndex > 4:
 		get_tree().change_scene_to_file("res://scenes/main_screen.tscn")
 	$HelpImage.texture = \
