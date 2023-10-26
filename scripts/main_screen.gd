2b17e481fd079b23a2f4ea380670781b8d3af0e8
cxhuy <alvinkang1@gmail.com>
2023-10-26 00:30:27 -0500

    feat: link how to play button

--- scripts/main_screen.gd
+++ scripts/main_screen.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
 
-func _on_button_pressed():
+func _on_game_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	
+
+func _on_help_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/help.tscn")
