95d6ecdd0df9e9fb259a894dd1e6977b1e92c059
cxhuy <alvinkang1@gmail.com>
2023-10-26 07:03:54 -0500

    feat: fix tank full being printed over cooldown

--- scripts/environment/tankFullLabel.gd
+++ scripts/environment/tankFullLabel.gd
@@ -4,7 +4,8 @@ extends Label
 func _process(delta):
 	if get_tree().get_nodes_in_group("TankBalls").size() > Data.tankLimit:
 		get_tree().get_nodes_in_group("TankBalls")[0].queue_free()
-	elif get_tree().get_nodes_in_group("TankBalls").size() == Data.tankLimit:
+	elif get_tree().get_nodes_in_group("TankBalls").size() == Data.tankLimit and \
+	!get_node("../TankCooldownLabel").inCooldown:
 		self.show()
 	else:
 		self.hide()
