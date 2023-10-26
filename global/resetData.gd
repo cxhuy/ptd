4b1630b8a4185f42bb650aac190c2e0183bf0738
cxhuy <alvinkang1@gmail.com>
2023-10-26 03:33:07 -0500

    feat: add reset global data script

---
+++ global/resetData.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var currentStage: int
+var currentWave: int
+var currentHealth: int
+
+var towerLimit: int
+var ballDamage: int
+var tankLimit: int
+
+var towerData: Dictionary
+
+
+func _ready():
+	currentStage = Data.currentStage
+	currentWave = Data.currentWave
+	currentHealth = Data.currentHealth
+	
+	towerLimit = Data.towerLimit
+	ballDamage = Data.ballDamage
+	tankLimit = Data.tankLimit
+	
+	towerData = Data.towerData
+
+
+func resetData():
+	Data.currentStage = currentStage
+	Data.currentWave = currentWave
+	Data.currentHealth = currentHealth
+	
+	Data.towerLimit = towerLimit
+	Data.ballDamage = ballDamage
+	Data.tankLimit = tankLimit
+	
+	Data.towerData = towerData
