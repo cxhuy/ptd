63d87b5af1de7152c346ff221888f9f600492372
cxhuy <alvinkang1@gmail.com>
2023-10-26 04:18:08 -0500

    fix: fix tank cooldown going down during break

--- scripts/environment/tankCooldownLabel.gd
+++ scripts/environment/tankCooldownLabel.gd
@@ -9,12 +9,11 @@ func startCooldown():
 	timeLeft = 30
 	inCooldown = true
 	
-	for i in range(30):
+	while timeLeft > 0:
 		self.text = str(timeLeft)
-		timeLeft -= 1
+		if get_parent().get_parent().inGame:
+			timeLeft -= 1
 		await get_tree().create_timer(1).timeout
 	
 	self.hide()
 	inCooldown = false
-	
-	
